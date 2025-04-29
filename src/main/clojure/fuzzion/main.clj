7251e56a56a4d44c6ca456f31b7472140eb768c5
Ilshat Sultanov <ilshat@sultanov.team>
2025-04-29 18:57:36 +0300

    Refactor main fn

--- src/main/clojure/fuzzion/main.clj
+++ src/main/clojure/fuzzion/main.clj
@@ -1,16 +1,13 @@
 (ns fuzzion.main
   (:require
-    [fuzzion.cli :as cli]))
-
-
-(defn run
-  [opts]
-  (cond
-    (:help opts) (println cli/help)
-    (:version opts) (println cli/version)
-    :else (println (pr-str opts))))
+    [fuzzion.cli :as cli]
+    [fuzzion.runner :as runner]))
 
 
 (defn -main
   [& args]
-  (run (:opts (cli/parse args))))
+  (let [{:keys [opts]} (cli/parse args)]
+    (cond
+      (:help opts) (println cli/help)
+      (:version opts) (println cli/version)
+      :else (runner/run opts))))
