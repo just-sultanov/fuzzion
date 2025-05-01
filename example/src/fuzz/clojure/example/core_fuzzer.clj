904d667c26a232aa7124c2ad626298680748884c
Ilshat Sultanov <ilshat@sultanov.team>
2025-05-01 15:43:35 +0300

    Add missing ns

---
+++ example/src/fuzz/clojure/example/core_fuzzer.clj
@@ -0,0 +1,19 @@
+(ns example.core-fuzzer
+  (:require
+    [example.core :as sut]
+    [fuzzion.core :as f]))
+
+
+(f/deftarget parse-json
+  [input]
+  (try
+    (sut/parse-json (f/consume-remaining-as-string input))
+    (catch Exception _)))
+
+
+(f/deftarget square
+  [input]
+  (try
+    (when (= 4 (sut/square (f/consume-long input)))
+      (throw (f/issue :high "You are found a bug")))
+    (catch ArithmeticException _)))
